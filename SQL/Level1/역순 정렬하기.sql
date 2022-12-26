ff748e2fdbd6232c195e3a8db3794192026facf1
bm <bm941201@gmail.com>
2022-12-26 23:16:53 +0900

    2022.12.26

---
+++ SQL/Level1/역순 정렬하기.sql
@@ -0,0 +1,7 @@
+SELECT NAME, DATETIME
+  FROM animal_ins
+ ORDER BY animal_id Desc
+
+-- 오름차순 정리를 하고 싶은 경우 ORDER BY에 ASC 추가
+-- 내림차순 정리를 하고 싶은 경우 ORDER BY에 DESC 추가
+-- ORDER BY를 여러개 하는 경우 제일 뒤의 요소부터 정렬함
