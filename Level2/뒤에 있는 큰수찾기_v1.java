e781bc7c8b74ffa913c27192acd5c41aa9e38b75
BM1201 <87167038+bm1201@users.noreply.github.com>
2023-01-31 17:27:12 +0900

    2023.01.31

---
+++ Level2/뒤에 있는 큰수찾기_v1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] solution(int[] numbers) {
+        int[] answer = new int[numbers.length];
+        
+        for(int i=0; i<numbers.length; i++){
+            if(i == numbers.length-1){
+                answer[i] = -1;
+            }
+            
+            for(int j=i+1; j<numbers.length; j++){
+                if(numbers[i] < numbers[j]){
+                    answer[i] = numbers[j];
+                    break;
+                }else{
+                    if(j == numbers.length-1){
+                        answer[i] = -1;
+                    }
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
